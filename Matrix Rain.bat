28ab9bedc1c705bb4cd5f888b44c9e50ac32d7d9
Hari Krishna Reddy <harireddy05@gmail.com>
2019-02-08 19:28:47 -0600

    Genesis
    Initial Commit

---
+++ Matrix Rain.bat
@@ -0,0 +1,47 @@
+@echo off
+color 0a
+title Matrix Console
+mode 1000
+echo.
+echo Welcome Hari...
+ping localhost -n 4 > nul
+echo.
+echo The shadows long for your return !
+ping localhost -n 7 > nul
+echo.
+echo Keypress to begin... 
+PAUSE >nul
+:START
+
+ping localhost -n 0 > nul
+echo								o	
+ping localhost -n 0 > nul
+echo		0						L
+ping localhost -n 0 > nul
+echo		3						e
+ping localhost -n 0 > nul
+echo		1						p									1
+ping localhost -n 0 > nul
+echo		I						W									0
+ping localhost -n 0 > nul
+echo	 u	2						O									1
+ping localhost -n 0 > nul
+echo	 a							r									1
+ping localhost -n 0 > nul
+echo	 D							e									0							K
+ping localhost -n 0 > nul
+echo	 ,							#						J			0							J
+ping localhost -n 0 > nul
+echo	 V					A		\						R			\							X
+ping localhost -n 0 > nul
+echo	 C					9								4			3							D
+ping localhost -n 0 > nul
+echo	 W					5								8										S
+ping localhost -n 0 > nul
+echo	 					1					3			W										Q
+ping localhost -n 0 > nul
+echo						Z					E			0										
+ping localhost -n 0 > nul
+echo						4										
+ping localhost -n 0 > nul
+GOTO START
